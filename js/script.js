8f9211e3359a2732ae216f00f40d2b74644288e0
LivingtonMerello98 <livington.merello@gmail.com>
2024-05-21 16:45:52 +0200

    [upd] prev-slide and next-slide and loop set to js

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 const slides = [
-{
-    image: 'img/01.webp',
+    {
+        image: 'img/01.webp',
         title: 'Marvel\'s Spiderman Miles Morale',
         text: 'Experience the rise of Miles Morales as the new hero masters incredible, explosive new powers to become his own Spider-Man.',
     }, {
@@ -10,7 +10,7 @@ const slides = [
     }, {
         image: 'img/03.webp',
         title: 'Fortnite',
-        text: "Grab all of your friends and drop into Epic Games Fortnite, a massive 100 - player face - off that combines looting, crafting, shootouts and chaos.",
+        text: "Grab all of your friends and drop into Epic Games Fortnite, a massive 100-player face-off that combines looting, crafting, shootouts and chaos.",
     }, {
         image: 'img/04.webp',
         title: 'Stray',
@@ -22,17 +22,34 @@ const slides = [
     }
 ];
 
-console.log(slides);
-
-const { createApp } = Vue
+// Eseguo la destructuring dell'oggetto Vue per estrarre la funzione createApp
+const { createApp } = Vue;
 
 createApp({
-  data() {
-    return {
-    slide: slides,
-    count: 0
-    }
-  }
-}).mount('#app')
+    // Definisco i dati della mia applicazione
+    data() {
+        return {
+            // Definisco l'array di diapositive
+            slide: slides,
+            // Imposto l'indice dell'immagine correntemente visualizzata a 0
+            currentIndex: 0
+        }
+    },
+    // Definisco i metodi che userò nell'applicazione
+    methods: {
+        // Definisco un metodo per visualizzare la prossima diapositiva
+        nextSlide() {
+            // Incremento l'indice corrente e assicuro che rimanga nell'intervallo delle diapositive
 
+            //l'operatore modulo ci permette di creare un loop tra le diapositive, facendo tornare l'indice al primo elemento dopo aver raggiunto l'ultimo elemento.
+            this.currentIndex = (this.currentIndex + 1) % this.slide.length;
+        },
+        // Definisco un metodo per visualizzare la diapositiva precedente
+        prevSlide() {
+            // Decremento l'indice corrente e assicuro che rimanga nell'intervallo delle diapositive
+            this.currentIndex = (this.currentIndex - 1 + this.slide.length) % this.slide.length;
+        }
+    }
+})
+.mount('#app');
 
